5a1026a8bbcad2ffe596fdf22c0e09f078e59bd1
yufei-ilariahuang <fayyyufei@gmail.com>
2025-01-31 15:31:02 -0800

    homework

--- workshop/ws2/ws2-3.sql
+++ workshop/ws2/ws2-3.sql
@@ -1,1 +1,15 @@
 # Homework ws2-3.sql
+SELECT 
+    s1.employee_id,
+    s1.product_id,
+    s1.sales
+FROM Sales s1
+WHERE (s1.sales, s1.product_id) = (
+    SELECT sales, MIN(product_id)
+    FROM Sales s2
+    WHERE s2.employee_id = s1.employee_id
+    GROUP BY sales
+    ORDER BY sales DESC
+    LIMIT 1
+)
+ORDER BY s1.employee_id;
--- workshop/ws3/ws3-3.sql
+++ workshop/ws3/ws3-3.sql
@@ -1,1 +1,5 @@
 # Homework ws3-3.sql
+SELECT student_id
+FROM Student s
+GROUP BY student_id
+HAVING COUNT(DISTINCT professor_key) = (SELECT COUNT(*) FROM Professor);
